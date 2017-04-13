abd1921930c0ef81fb7da1979db12274ddd43551
grensley <grensley@gmail.com>
2017-04-12 19:41:17 -0600

    initial commit

---
+++ cocktail/index.ios.js
@@ -0,0 +1,62 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  ListView,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+export default class cocktail extends Component {
+  
+  constructor(props) {
+    var cocktails = require('./cocktails.json');
+    var cocktailNames = []
+    for (ct in cocktails) {
+      console.log(cocktails[ct])
+      cocktailNames.push(cocktails[ct].name)
+    }
+    super(props);
+    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    this.state = {
+      dataSource: ds.cloneWithRows(cocktailNames)
+    };
+  }
+  render() {
+    return (
+      <View style={{flex: 1, paddingTop: 22}}>
+        <ListView
+          dataSource={this.state.dataSource}
+          renderRow={(rowData) => <Text>{rowData}</Text>}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('cocktail', () => cocktail);
